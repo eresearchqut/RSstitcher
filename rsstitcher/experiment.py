837fea756de5773b4d629c24dc0df1958ce26d2d
ScriptSmith <ScriptSmith@users.noreply.github.com>
2026-04-01 12:10:30 +1000

    Revert "Double the radial step by using half of delta_s"
    This reverts commit 023e2e39d3b807ba0fcb32a490661a56c781f73e.

--- rsstitcher/experiment.py
+++ rsstitcher/experiment.py
@@ -470,13 +470,11 @@ def _build_polar_grid(
         }
     )
 
-    # R and Gamma grids derived from positions (using delta_s/2 as R step for
-    # higher resolution 1D profiles)
-    r_step = delta_s / 2
+    # R and Gamma grids derived from positions (using delta_s as R step)
     out_r = np.arange(
-        round(float(positions["R"].min()), n_decimals) - r_step,
-        round(float(positions["R"].max()), n_decimals) + r_step,
-        r_step,
+        round(float(positions["R"].min()), n_decimals) - delta_s,
+        round(float(positions["R"].max()), n_decimals) + delta_s,
+        delta_s,
     )
     delta_gamma = np.radians(0.5)
     out_gamma = np.arange(
